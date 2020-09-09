3da08aefeb2095d22d28049d9d7f8a03591a3514
AlexandrNikolaichev <saaacha.nik@yandex.ru>
2020-09-09 21:51:11 +0300

    fix path

--- docs/precache-manifest.e4246066232d0c8b9213c5f531921e21.js
+++ docs/precache-manifest.e4246066232d0c8b9213c5f531921e21.js
@@ -5,66 +5,66 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "eb430d7e2e2282aec484",
-    "url": "/static/css/main.a1107254.chunk.css"
+    "url": "/test-vk-app/static/css/main.a1107254.chunk.css"
   },
   {
     "revision": "3653dceec2bb1395e4ce",
-    "url": "/static/js/2.84911def.chunk.js"
+    "url": "/test-vk-app/static/js/2.84911def.chunk.js"
   },
   {
     "revision": "41b66b6fd6eecba581340794bb1831be",
-    "url": "/static/js/2.84911def.chunk.js.LICENSE.txt"
+    "url": "/test-vk-app/static/js/2.84911def.chunk.js.LICENSE.txt"
   },
   {
     "revision": "eb430d7e2e2282aec484",
-    "url": "/static/js/main.f7857959.chunk.js"
+    "url": "/test-vk-app/static/js/main.f7857959.chunk.js"
   },
   {
     "revision": "df11726cbf66339790ea",
-    "url": "/static/js/runtime-main.970cab50.js"
+    "url": "/test-vk-app/static/js/runtime-main.970cab50.js"
   },
   {
     "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
-    "url": "/static/media/1.dfc54362.png"
+    "url": "/test-vk-app/static/media/1.dfc54362.png"
   },
   {
     "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
-    "url": "/static/media/2.20489759.png"
+    "url": "/test-vk-app/static/media/2.20489759.png"
   },
   {
     "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
-    "url": "/static/media/3.dbf82c38.png"
+    "url": "/test-vk-app/static/media/3.dbf82c38.png"
   },
   {
     "revision": "e671d53961f44185ee61256a0e6f13a5",
-    "url": "/static/media/4.e671d539.png"
+    "url": "/test-vk-app/static/media/4.e671d539.png"
   },
   {
     "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
-    "url": "/static/media/burger.35bcd067.png"
+    "url": "/test-vk-app/static/media/burger.35bcd067.png"
   },
   {
     "revision": "06277842ccc3f7c2a37085189dacdcbc",
-    "url": "/static/media/check-symbol.06277842.svg"
+    "url": "/test-vk-app/static/media/check-symbol.06277842.svg"
   },
   {
     "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
-    "url": "/static/media/edit.751f84c2.svg"
+    "url": "/test-vk-app/static/media/edit.751f84c2.svg"
   },
   {
     "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
-    "url": "/static/media/kfc.4fd2ba47.png"
+    "url": "/test-vk-app/static/media/kfc.4fd2ba47.png"
   },
   {
     "revision": "60464810e8cfcde7a20fc19284c38a0e",
-    "url": "/static/media/mcdac.60464810.png"
+    "url": "/test-vk-app/static/media/mcdac.60464810.png"
   },
   {
     "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
-    "url": "/static/media/refresh-button.1198ed41.svg"
+    "url": "/test-vk-app/static/media/refresh-button.1198ed41.svg"
   },
   {
     "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
-    "url": "/static/media/sub.4eb05fdd.png"
+    "url": "/test-vk-app/static/media/sub.4eb05fdd.png"
   }
 ]);
